15bfa2641ded8326846998d149b1ed931d70fced
akiencore <akiencore@qq.com>
2018-06-12 22:25:57 -0400

    iter0-uncomp?

--- src/TFTPClient.java
+++ src/TFTPClient.java
@@ -4,8 +4,9 @@ import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.SocketException;
 import java.net.UnknownHostException;
+import java.util.Scanner;
 
-public class TFTPClient {
+public class TFTPClient{
 
 	   DatagramPacket sendPacket, receivePacket;
 	   DatagramSocket sendReceiveSocket;
@@ -14,7 +15,7 @@ public class TFTPClient {
 	   private static final int SERVER_PORT = 69;
 		
 	   private static final int DATA_SIZE = 516;
-		
+	
 	   public TFTPClient() {
 		   try {
 			   sendReceiveSocket = new DatagramSocket();
@@ -25,10 +26,42 @@ public class TFTPClient {
 	   }
 	   
 	   public void TFTPSendAndReceive() {
+		   byte[] fn, md;
+		   String filename, mode; 
+		   
 		   String s = "Anyone there?";
 		   System.out.println("Client: sending a packet containing:\n" + s);
 		   
-		   byte[] msg = s.getBytes();
+
+		   System.out.println("The request is a: ");
+		   @SuppressWarnings("resource")
+		   Scanner scanner = new Scanner(System.in);
+		   String pattern = scanner.nextLine().toLowerCase();
+		   
+		   byte opCode = -1;
+		   if(pattern.equals("rrq") || pattern.equals("read")){
+			   opCode = 1;
+		   } else if (pattern.equals("wrq") || pattern.equals("write")){
+			   opCode = 2;
+		   } else {
+			   System.out.println("Invalid request, exit");
+			   System.exit(1);
+		   }
+		   
+		   filename = s;
+		   fn = filename.getBytes();
+		   
+		   mode = "octet";
+		   md = mode.getBytes();
+		   
+		   byte[] msg = new byte[DATA_SIZE];
+		   System.arraycopy(fn, 0, msg, 2, fn.length);
+		   msg[fn.length + 2] = 0;
+		   System.arraycopy(md, 0, msg, fn.length + 3, md.length);
+		   int len = (fn.length + md.length) + 4;
+		   msg[len - 1] = 0; // fn.length + 4 - 1 -> last digit
+		   msg[0] = opCode;
+		   msg[1] = opCode;
 		   
 		   try {
 			   sendPacket = new DatagramPacket(msg, msg.length, 
@@ -38,47 +71,62 @@ public class TFTPClient {
 			   System.exit(1);
 		   }
 		   
-		   System.out.println("Client: Sending Packet: ");
+		   System.out.println("Client-Sending Packet: ");
 		   System.out.println("To Host: " + sendPacket.getAddress());
 		   System.out.println("To Port: " + sendPacket.getPort());
-		   int len = sendPacket.getLength();
 		   System.out.println("Length: " + len);
 		   System.out.print("Containing: ");
-		   String sending = new String(sendPacket.getData(),0,len);
+		   String sending = s;
 		   System.out.println(sending);
 		   
-		   try {
-			   sendReceiveSocket.send(sendPacket);
-		   } catch(IOException e) {
-			   e.printStackTrace();
-			   System.exit(1);
-		   }
+		   sendToServer(sendReceiveSocket, sendPacket);
 		   
-		   System.out.println("Client: Packet Sent.\n");
+		   System.out.println("Client-Packet Sent.\n");
+		   	   
 		   
 		   byte[] data = new byte[DATA_SIZE];
 		   receivePacket = new DatagramPacket(data, data.length);
 		   
-		   try {
-			   sendReceiveSocket.receive(receivePacket);
-		   } catch (IOException e) {
-			   e.printStackTrace();
-			   System.exit(1);
-		   }
+		   receiveFromServer(sendReceiveSocket, sendPacket);
 		   
-		   System.out.println("Client: Receiving Packet: ");
-		   System.out.println("To Host: " + receivePacket.getAddress());
-		   System.out.println("To Port: " + receivePacket.getPort());
+		   System.out.println("Client-Receiving Packet: ");
+		   System.out.println("From Host: " + receivePacket.getAddress());
+		   System.out.println("From Port: " + receivePacket.getPort());
 		   len = receivePacket.getLength();
 		   System.out.println("Length: " + len);
 		   System.out.print("Containing: ");
 		   String receiving = new String(data,0,len);
 		   System.out.println(receiving);
+
+		   opCode = data[1];
+		   if(opCode==4)
+			   System.out.println("Request acknowledged");
 		   
 		   sendReceiveSocket.close();
 	   }
 	   
-	   public void main(String[] args) {
+	   public static void sendToServer(DatagramSocket socket, DatagramPacket packet) {
+		   try {
+			   socket.send(packet);
+		   } catch (IOException e) {
+			   e.printStackTrace();
+			   System.exit(1);
+		   }
+	   }
+	   
+	   public static void receiveFromServer(DatagramSocket socket, DatagramPacket packet) {
+		   try {
+				System.out.println("Waiting...");
+				socket.receive(packet);
+			} catch(IOException e) {
+				System.out.print("IO Exception: likely: ");
+				System.out.println("Receive Socket Timed Out.\n" + e);
+				e.printStackTrace();
+				System.exit(1);
+			}
+	   }
+	   
+	   public static void main(String[] args) {
 		   TFTPClient client = new TFTPClient();
 		   client.TFTPSendAndReceive();
 	   }
--- src/TFTPServer.java
+++ src/TFTPServer.java
@@ -1,11 +1,15 @@
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.SocketException;
+import java.net.UnknownHostException;
 
 public class TFTPServer {
 	DatagramPacket sendPacket, receivePacket;
 	DatagramSocket sendSocket, receiveSocket;
+	
+	private String serverFolder = System.getProperty("user.dir") + "server_files";
 	
 	private static final int SERVER_PORT = 69;
 	
@@ -23,7 +27,7 @@ public class TFTPServer {
 		}
 	}
 	
-	public void TFTPReceiveAndSend() {
+	public void TFTPReceiveAndSend() throws UnknownHostException {
 		byte data[] = new byte[DATA_SIZE];
 		receivePacket = new DatagramPacket(data, data.length);
 		System.out.println("Server: wait for packet.\n");
@@ -38,7 +42,7 @@ public class TFTPServer {
 			System.exit(1);
 		}
 		
-		System.out.println("Server: Packet Received: ");
+		System.out.println("Server-Packet Received: ");
 		System.out.println("From Host: " + receivePacket.getAddress());
 		System.out.println("From Port: " + receivePacket.getPort());
 		int len = receivePacket.getLength();
@@ -55,16 +59,58 @@ public class TFTPServer {
 			System.exit(1);
 		}
 		
-		sendPacket = new DatagramPacket(data, receivePacket.getLength(), 
-				receivePacket.getAddress(), receivePacket.getPort());
+
+		byte opCode = -1;
+		byte blockNum = -1;
+		byte[] msg = new byte[DATA_SIZE];
+		
+		String r = "Null";
+		
+		if(data[1] == 1) { //RRQ
+			System.out.println("RRQ from the client");
 	
-		System.out.println("Server: Sending Packet: ");
+			blockNum = 0;
+			opCode = 3;
+			r = "Server is here.";
+			byte[] rn = r.getBytes();
+			
+			//System.arraycopy(blockNum, 0, msg, 2, 1); //problem
+			System.arraycopy(rn, 0, msg, 4, rn.length); //problem??
+			
+			msg[1] = opCode;
+			msg[3] = blockNum;
+
+			len = 4 + rn.length;
+			
+		} else if(data[1]==2) { //WRQ
+			System.out.println("WRQ from the client");
+			
+			blockNum = 0;
+			opCode = 4;
+			
+			//System.arraycopy(blockNum, 0, msg, 2, 1); //problem
+			msg[0] = 0;
+			msg[1] = opCode;
+			msg[2]= 0;
+			msg[3] = blockNum;
+			
+			len = 4;
+		}
+		
+		try {
+			sendPacket = new DatagramPacket(msg, msg.length, 
+					InetAddress.getLocalHost(), SERVER_PORT);
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	
+		System.out.println("Server-Sending Packet: ");
 		System.out.println("To Host: " + sendPacket.getAddress());
-		System.out.println("To Port: " + sendPacket.getPort());
-		len = sendPacket.getLength();
+		System.out.println("To Port: " + SERVER_PORT);
 		System.out.println("Length: " + len);
 		System.out.print("Containing: ");
-		String sending = new String(sendPacket.getData(),0,len);
+		String sending = r;
 		System.out.println(sending + "\n");
 		
 		try {
@@ -74,14 +120,14 @@ public class TFTPServer {
 			System.exit(1);
 		}
 		
-	    System.out.println("Server: packet sent");
+	    System.out.println("Server-packet sent");
 	    
 	    sendSocket.close();
 	    receiveSocket.close();
 	}
 	
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws UnknownHostException {
 		TFTPServer server = new TFTPServer();
 		server.TFTPReceiveAndSend();
 	}
